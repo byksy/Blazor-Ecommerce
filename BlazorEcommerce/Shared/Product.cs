2c7610dc05784a9d3b6cfe655092f41941dff418
byksy <ayisan1096@gmail.com>
2022-04-26 23:28:15 +0300

    Add Product Model and razor page

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -11,7 +11,7 @@ namespace BlazorEcommerce.Shared
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty ;
-        public string Image { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
         public decimal Price { get; set; }
     }
 }
